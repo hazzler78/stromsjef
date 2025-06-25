3024b59824d2153be4007a9232ef121bcc5f22c0
Mikael <hazzler@gmail.com>
2025-06-25 09:19:44 +0200

    Update GTM implementation to use environment variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 export const metadata: Metadata = {
   title: "Strømsjef - Sammenlign strømavtaler",
   description: "Sammenlign strømavtaler og bytt strømleverandør for å spare penger.",
@@ -19,32 +21,36 @@ export default function RootLayout({
     <html lang="no">
       <head>
         {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N6Z4244M');
-            `,
-          }}
-        />
+        {GTM_ID && (
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+            }}
+          />
+        )}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-              <!-- Google Tag Manager (noscript) -->
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6Z4244M"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-              <!-- End Google Tag Manager (noscript) -->
-            `,
-          }}
-        />
+        {GTM_ID && (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
+                <!-- Google Tag Manager (noscript) -->
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                <!-- End Google Tag Manager (noscript) -->
+              `,
+            }}
+          />
+        )}
         <Header />
         <main className="container mx-auto p-4">{children}</main>
         <Footer />
